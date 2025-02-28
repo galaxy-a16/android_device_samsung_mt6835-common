7d7d322d1646b97f6de65ed5cd6038b499020ef9
Jamie Falconer <mrfluffyoven@gmail.com>
2025-02-28 14:48:56 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# android_device_samsung_mt6835-common
---
+++ twrp_mt6835-common.mk
@@ -0,0 +1,11 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit from mt6789-common
+$(call inherit-product, device/samsung/mt6835-common/device.mk)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/samsung/mt6835-common/recovery/root,recovery/root)
